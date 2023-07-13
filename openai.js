a277ac651a51efd12f58aa5a600a3a1778300730
Yaniv Yehuda <yaniv@deweb.io>
2023-07-13 17:22:37 +0300

    allow 25MB file

--- openai.js
+++ openai.js
@@ -6,7 +6,10 @@ const apiKey = fs.readFileSync('openai_key.text', 'utf-8').trimEnd();
 async function transcribe(file, outputPath, format='text') {
   try {
         let openai = new OpenAIApi(new Configuration({apiKey}));
-        const transcription = await openai.createTranscription(fs.createReadStream(file), 'whisper-1', undefined, format);
+        const transcription = await openai.createTranscription(
+            fs.createReadStream(file), 'whisper-1', undefined, format, undefined, undefined, 
+            {maxBodyLength: 25 * 1024 * 1024}); // maxBodyLength - axios config to be able to work with files up to 25MB (maximum size for whispher).
+        console.log(transcription);
         fs.appendFileSync(outputPath, format == 'text' || format === 'srt' ? transcription.data : transcription);
     } catch(err) {
         console.error('error!' + err.message)
